274c033b7380d7a561fa844fe450bf1f5712ec3b
seongyeol.kim <jsdoublea@gmail.com>
2023-11-27 11:54:37 +0900

    feat/VTL-4 Jest 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,10 +32,11 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
-    'no-unused-vars': 0, // typescript의 no-unused-vars 사용
-    'class-methods-use-this': 0,
-    '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-unused-vars': ['error'],
+    'no-unused-vars': 0, // typescript의 no-unused-vars 사용
+    'class-methods-use-this': 0, // 필수적으로 선언되어야 하나 구현이 필요없는 경우가 있어 해제
+    '@typescript-eslint/no-explicit-any': 0,
     'comma-dangle': ['error', 'always-multiline'],
   },
+  ignorePatterns: ['jest.config.ts'],
 };
